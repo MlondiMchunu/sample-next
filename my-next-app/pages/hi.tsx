daef2efa48a749a537e1b8c8f4c2009971d1c417
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-03-31 22:49:38 +0200

    commit next/head

--- my-next-app/pages/hi.tsx
+++ my-next-app/pages/hi.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Hi: NextPage = () => {
-    return (<>Hello World!</>)
+    return (
+        <div>
+            <Head>
+                <title>Hello World Page Title</title>
+                <meta property="og:title" content="Hello World" key="title" />
+            </Head>
+            <>Hello World!</>
+        </div>)
 };
 
 export default Hi;
