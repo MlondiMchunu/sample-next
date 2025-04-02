ec7be4ea2a7e94e210e9b7e7bfd11e7413f487f6
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-02 13:53:44 +0200

    declare api response type

--- my-next-app/pages/names-isr.tsx
+++ my-next-app/pages/names-isr.tsx
@@ -1,7 +1,15 @@
-import type{
+import type {
     GetStaticProps,
     GetStaticPropsContext,
     InferGetStaticPropsType,
     NextPage,
     PreviewData
 } from "next";
+
+import {ParsedUrlQuery} from "querystring";
+import {fetchNames} from "../utils/fetch-names";
+
+type responseItemType = {
+    id:string,
+    name:string
+};
