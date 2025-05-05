3c7a7c82837b1809fed5e9e285dd1f94bf78db4a
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-05 20:24:25 +0200

    define query and mutation resolver

--- my-next-app/graphql/resolvers.ts
+++ my-next-app/graphql/resolvers.ts
@@ -7,3 +7,16 @@ declare interface WeatherInterface {
     tempF: string;
     friends: string[];
 }
+
+export const resolvers = {
+    Query: {
+        weather: async (_: any, param: WeatherInterface) => {
+            return [db.find((item) => item.zip === param.zip)];
+        }
+    },
+    Mutation: {
+        weather: async (_: any, param: { data: WeatherInterface }) => {
+            return [db.find((item) => item.zip === param.data.zip)];
+        }
+    }
+};
