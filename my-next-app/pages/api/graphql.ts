a5de856eae7974b9659c1ee1254222524feceaaf
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-06 01:26:44 +0200

    initialize new GraphQL server with typeDefs and resolvers

--- my-next-app/pages/api/graphql.ts
+++ my-next-app/pages/api/graphql.ts
@@ -2,4 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { resolvers } from "../../graphql/resolvers";
 import { typeDefs } from "../../graphql/schema";
-import { NextApiHandler, NextApiRequest,NextApiResponse } from "next";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+//@ts-ignore
+const server = new ApolloServer({
+    resolvers,
+    typeDefs
+});
