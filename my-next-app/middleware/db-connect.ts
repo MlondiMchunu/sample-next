9e3afeb28bae05390fac4db85a0c2d30e3acccc5
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-09 01:40:26 +0200

    import mongoose package and the mongodb-memory-server database

---
+++ my-next-app/middleware/db-connect.ts
@@ -0,0 +1,2 @@
+import mongoose from "mongoose";
+import {MongoMemoryServer} from "mongodb-memory-server";
--- my-next-app/mongoose/weather/model.ts
+++ my-next-app/mongoose/weather/model.ts
@@ -2,4 +2,4 @@ import mongoose, { model } from "mongoose";
 import { WeatherInterface } from "./interface";
 import { WeatherSchema } from "./schema";
 
-export default mongoose.models.Weather || model <WeatherInterface>("Weather",WeatherSchema);
+export default mongoose.models.Weather || model<WeatherInterface>("Weather", WeatherSchema);
