dd233c3173b4dc37d78ab4e21f55e2509905810c
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-09 02:53:01 +0200

    import necessary dependencies to [...zipcode.ts]

--- my-next-app/pages/api/weather/[...zipcode].ts
+++ my-next-app/pages/api/weather/[...zipcode].ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { findByZip } from "@/mongoose/weather/services";
+import dbConnect from "@/middleware/db-connect";
 
 type WeatherDetailType = {
     zipcode: string;
