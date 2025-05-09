196ace5d7f1908bbd8b8d4b02a3aa23909ae6e12
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-09 02:12:23 +0200

    create and export storeDocument function

--- my-next-app/mongoose/weather/services.ts
+++ my-next-app/mongoose/weather/services.ts
@@ -1,2 +1,11 @@
 import WeatherModel from "./model";
-import {WeatherInterface} from "./interface";
+import { WeatherInterface } from "./interface";
+
+export async function storeDocument(doc: WeatherInterface): Promise<boolean> {
+    try {
+        await WeatherModel.create(doc);
+    }catch(error){
+        return false;
+    }
+    return true
+}
