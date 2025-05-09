9a8c1597db3894b304d27766bcfa8ccae8c8d75a
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-09 02:57:36 +0200

    modify the API to take a query parameter from the url

--- my-next-app/pages/api/weather/[...zipcode].ts
+++ my-next-app/pages/api/weather/[...zipcode].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { findByZip } from "@/mongoose/weather/services";
 import dbConnect from "@/middleware/db-connect";
+dbConnect();
 
 type WeatherDetailType = {
     zipcode: string;
@@ -12,9 +13,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
+    let data = await findByZip(req.query.zipcode as string);
     return res.status(200).json({
-        zipcode: req.query.zipcode,
-        weather: "sunny",
-        temp: 35
+        data
+        //zipcode: req.query.zipcode,
+        //weather: "sunny",
+        //temp: 35
     });
 }
