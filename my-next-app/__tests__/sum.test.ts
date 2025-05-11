04dedddd5ad259e0f67ac0dc22370a2afb2079e4
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-11 15:03:12 +0200

    test steps act

--- my-next-app/__tests__/sum.test.ts
+++ my-next-app/__tests__/sum.test.ts
@@ -1,16 +1,20 @@
 import { sum } from "../helpers/sum";
 
-describe("the sum function", () => {
+describe("the sum function", () => {//describe function creates test suit
     test("two plus two is four", () => {
         let first = 2;
         let second = 2;
         let expectation = 4;
+
+        let result = sum(first, second);
     });
 
     test("minus eight plus four is minus four", () => {
         let first = -8;
         let second = 4;
         let expectation = -4;
+
+        let result = sum(first, second);
     });
 
 });
