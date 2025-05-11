c1490d2f5deb192c6dab1110341ce03a60626789
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-11 14:30:21 +0200

    define first test case

--- my-next-app/__tests__/sum.test.ts
+++ my-next-app/__tests__/sum.test.ts
@@ -1,5 +1,10 @@
 import { sum } from "../helpers/sum";
 
-describe("the sum function",()=>{
+describe("the sum function", () => {
+    test("two plus two is four", () => {
+        let first = 2;
+        let second = 2;
+        let expectation = 4;
+    });
 
 });
