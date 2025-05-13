e2f2042fbddbbb5b9bfa794d030f23d6a9c1c0d5
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-05-13 12:20:04 +0200

    create a stubbed function

--- my-next-app/__tests__/fibonacci.test.ts
+++ my-next-app/__tests__/fibonacci.test.ts
@@ -5,9 +5,9 @@ jest.mock("../helpers/sum");
 describe("the fibonacci sequence", () => {
 
     test("with a lenth of 0 is ", () => {
-        expect(fibonacci(0)).toBe(" ");
+        expect(fibonacci(0)).toBe("");
     });
-    test("with a lenth of 5 is ", () => {
+    test("with a lenth of 5 is '0, 1, 1, 2, 3'", () => {
         expect(fibonacci(5)).toBe("0, 1, 1, 2, 3");
     });
 });
---
+++ my-next-app/helpers/__mocks__/sum.ts
@@ -0,0 +1,1 @@
+export const sum = (data: number[]): number => 999;
